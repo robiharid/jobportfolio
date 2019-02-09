da37f0c62b0e386f5829d02ebd35c41422cebba9
brandon-schabel <brandonschabel1995@gmail.com>
2019-02-09 12:20:44 -0800

    Move firebase config into sep file, add form to create company

--- src/shared/index.tsx
+++ src/shared/index.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
 import Nav from "./components/Nav"
 import DisplayTable from './components/DisplayTable'
+import Form from './components/Form'
 
-export { Header, Nav, DisplayTable}
+export { Header, Nav, DisplayTable, Form }
