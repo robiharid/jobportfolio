958ab5d1dd971b5b5a660880a733f445ffe94627
brandon-schabel <brandonschabel1995@gmail.com>
2019-02-09 12:33:47 -0800

    connect form to firebase

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -3,7 +3,10 @@ import firebase from "firebase"
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.RREACT_APP_FIREBASE_DB
+  databaseURL: process.env.REACT_APP_FIREBASE_DB,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID 
 }; 
 
 firebase.initializeApp(config);
