236743a6dd3dc70357cb95b491bedb8aa321e8e1
brandon-schabel <brandonschabel1995@gmail.com>
2019-02-10 09:12:07 -0800

    WIP - displaying user data table data

--- src/shared/components/DisplayTable/interfaces.d.ts
+++ src/shared/components/DisplayTable/interfaces.d.ts
@@ -1,3 +1,15 @@
-interface TablePropsInterface {
+interface IProps {
   
+}
+
+interface IState { 
+  userCompanies: {
+    name: string,
+    email: string,
+    salary: number,
+    deadline: string,
+    location: string,
+    id: string
+  }[],
+  errorMessage: string
 }
